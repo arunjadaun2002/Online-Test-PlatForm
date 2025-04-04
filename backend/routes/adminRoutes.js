d9d117045bee5e72d76c476784b9652dff50df82
Rohit Kumar <krohitgupta1502@gmail.com>
2025-04-04 16:02:33 +0530

    Admin can reg std

--- backend/routes/adminRoutes.js
+++ backend/routes/adminRoutes.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const { adminSignup, adminLogin, adminForgotPassword, registerStudent, verifyAdmin } = require('../controllers/adminController');
-const authMiddleware = require('../middlewares/authMiddleware');
+const authentication = require('../middlewares/authMiddleware');
 
 const router = express.Router();
 
 router.post('/signup', adminSignup);
 router.post('/login', adminLogin);
 router.post('/forgot-password', adminForgotPassword);
-router.post('/register-student', authMiddleware, registerStudent);
+router.post('/register-student', authentication, registerStudent);
 router.get('/verify', verifyAdmin);
 
 module.exports = router;
